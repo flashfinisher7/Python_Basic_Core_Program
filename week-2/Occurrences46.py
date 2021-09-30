c1f663bd7343dc1c85417ef54fff938ca4ccddd3
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-09-30 15:55:32 +0530

    Feat: occurrences of specificed element in a array

---
+++ week-2/Occurrences46.py
@@ -0,0 +1,4 @@
+from array import *
+array_num = array('i', [1, 2, 3, 4, 5, 6, 7])
+print("Original array: "+str(array_num))
+print("Number of occurrences of the number 7 in the said array: "+str(array_num.count(7)))

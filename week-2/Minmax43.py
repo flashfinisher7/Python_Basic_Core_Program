6b8f90f060431a90abbc1dd52feed44726526c6e
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-09-30 15:52:53 +0530

    Feat: find max and min number from sequence of numbers

---
+++ week-2/Minmax43.py
@@ -0,0 +1,11 @@
+def max_min(data):
+  l = data[0]
+  s = data[0]
+  for num in data:
+    if num> l:
+      l = num
+    elif num< s:
+        s = num
+  return l, s
+
+print(max_min([0, 7, 17, 27, -7, 47, 37, 28, 77]))

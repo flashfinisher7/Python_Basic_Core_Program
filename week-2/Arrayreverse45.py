64c846bbe0e3f2399f2d3868b72df64bec6ebfc4
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-09-30 15:54:41 +0530

    Feat: reverse the order of items in the array

---
+++ week-2/Arrayreverse45.py
@@ -0,0 +1,6 @@
+from array import *
+array_num = array('i', [1, 2, 3, 4, 5, 6, 7, 77])
+print("Original array: "+str(array_num))
+array_num.reverse()
+print("Reverse the order of the items:")
+print(str(array_num))

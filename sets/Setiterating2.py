0ad9727b6e712065579e754080abf672230b6deb
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-09-30 11:31:46 +0530

    feat : iteration over set

---
+++ sets/Setiterating2.py
@@ -0,0 +1,9 @@
+#Create a set 
+num_set = set([0, 1, 2, 3, 4, 5])
+for n in num_set:
+  print(n, end=' ')
+print("\n\nCreating a set using string:")
+char_set = set("Anil")  
+# Iterating using for loop
+for val in char_set:
+    print(val, end=' ')

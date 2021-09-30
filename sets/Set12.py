191be5d69860d0dab0735755e902db9b2dac6ec5
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-09-30 11:38:17 +0530

    feat : find max min values in a set

---
+++ sets/Set12.py
@@ -0,0 +1,9 @@
+#Create a set
+setn = {7, 17, 27, 37, 47, 57}
+print("Original set elements:")
+print(setn)
+print(type(setn))
+print("\nMaximum value of the said set:")
+print(max(setn))
+print("\nMinimum value of the said set:")
+print(min(setn))

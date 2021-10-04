5969b0d2aff5aae5f4b903a99e1bb7f7f46f72fb
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-10-04 13:58:08 +0530

    [Anil] Feat : probabiity of drawing an ace from pack of cards

---
+++ probability/AceProb.py
@@ -0,0 +1,11 @@
+"""
+@Author: Anil
+@Date: 2021-10-01 01:00
+@Last Modified by: Anil
+@Last Modified time: 2021-10-01 01:05
+@Title : To find probability of drawing an ace from pack of cards
+"""
+cards=52
+aces=4
+probability_aces=aces/cards
+print("Probability of drawing Ace from pack of cards:",probability_aces)

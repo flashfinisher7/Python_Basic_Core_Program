5f9ae477186d85eef8b9593f6a57574241659765
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-10-05 19:07:17 +0530

    [Anil] feat : 3x3 matrix 2D replace

---
+++ Numpy/3x3matrix.py
@@ -0,0 +1,10 @@
+"""
+@Author: Anil
+@Date: 2021-10-05 09:40
+@Last Modified by: Anil
+@Last Modified time: 2021-10-05 09:49
+@Title : Create a 3x3 matrix with values ranging from 2 to 10
+"""
+import numpy as np
+x =  np.arange(2, 11).reshape(3,3)
+print(x)

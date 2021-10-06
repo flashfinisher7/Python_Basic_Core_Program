7c8b876f517db925e3f24880ed92c43cad345d2b
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-10-06 11:14:15 +0530

    [Anil] Feat : create 3Darray with ones on a diagonal and zeros

---
+++ Numpy/3Darray.py
@@ -0,0 +1,11 @@
+"""
+@Author: Anil
+@Date: 2021-10-06 12:00
+@Last Modified by: Anil
+@Last Modified time: 2021-10-06 12:05
+@Title : program to create a 3-D array with ones on a diagonal and zeros
+elsewhere
+"""
+import numpy as np
+x = np.eye(3)
+print(x)

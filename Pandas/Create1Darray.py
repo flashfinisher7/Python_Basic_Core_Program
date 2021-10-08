227d551ffb2ddde654c7ae2b18903a3bd153d4b3
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-10-08 11:02:44 +0530

    [Anil] Feat : Display one dimensional array like object contains array

---
+++ Pandas/Create1Darray.py
@@ -0,0 +1,11 @@
+"""
+@Author: Anil
+@Date: 2021-10-07   10:00
+@Last Modified by: Anil
+@Last Modified time: 2021-10-07   10:04 
+@Title : program to create and display a one-dimensional array-like object
+containing an array of data using Pandas module
+"""
+import pandas as pd
+OneDarray = pd.Series([1, 2, 3, 4, 5])
+print(OneDarray)

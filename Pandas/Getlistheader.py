94435135b0506b311205cb712dc6a29ce9f41e2a
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-10-08 11:27:22 +0530

    [Anil] Feat : Get list from dataframe column header

---
+++ Pandas/Getlistheader.py
@@ -0,0 +1,16 @@
+"""
+@Author: Anil
+@Date: 2021-10-08  10:20
+@Last Modified by: Anil
+@Last Modified time: 2021-10-08  10:30
+@Title :  program to get list from DataFrame column headers
+"""
+import pandas as pd
+import numpy as np
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data , index=labels)
+print(list(df.columns.values))

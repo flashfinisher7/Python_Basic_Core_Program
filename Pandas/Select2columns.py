d3f8da4295cbc2daf35c64c0c75f272c12752d54
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-10-08 11:09:58 +0530

    [Anil] Feat : select two columns in a dataframe

---
+++ Pandas/Select2columns.py
@@ -0,0 +1,19 @@
+"""
+@Author: Anil
+@Date: 2021-10-07   10:55
+@Last Modified by: Anil
+@Last Modified time: 2021-10-07   11:04
+@Title :  program to select the 'name' and 'score' columns from the following DataFrame.
+"""
+import pandas as pd
+import numpy as np
+
+student_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+result = pd.DataFrame(student_data , index=labels)
+print("Select specific columns:")
+print(result[['name', 'score']])
